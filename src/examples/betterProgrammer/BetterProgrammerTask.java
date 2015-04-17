965ef99ebc4a9015a72373f507184cf1493f9bf8
sb <2sbsbsb@gmail.com>
2015-04-16 20:45:18 -0700

    BetterProgrammer - getSumOfNumbers

--- src/examples/betterProgrammer/BetterProgrammerTask.java
+++ src/examples/betterProgrammer/BetterProgrammerTask.java
@@ -1,43 +1,22 @@
 package examples.betterProgrammer;
 
-import java.util.*;
 
 public class BetterProgrammerTask {
 
-	  public static double getProbability(int Y, int X) {
-	        /*
-	          If you roll Y standard six-sided dice, what’s the probability that you get at least X 4s?
-	          To calculate that you should divide the number of comibnations with X or more 4s
-	          by the total number of possible combinations.
-	         */		   	
-		  double sum = 0;
-		  for(int i=X;i<=Y;i++) {
-			 double value = nCr(Y,i) * Math.pow((1/6d),i) * Math.pow(5/6d, Y-i);
-			sum+= (value);
-		  }
-		  return sum;		   
-	    }
+	public static int getSumOfNumbers(String s) {
+		/*
+		 * Please implement this method to return the sum of all integers found
+		 * in the parameter String. You can assume that integers are separated
+		 * from other parts with one or more spaces (' ' symbol). For example,
+		 * s="12 some text 3  7", result: 22 (12+3+7=22)
+		 */
+		int sum = 0;
+		String[] strArrays = s.split(" ");
+		for (String str : strArrays) {
+			sum += Integer.parseInt(str);
+		}
+		return sum;
 
-	  
-	  /**
-	   * @param n
-	   * @param r
-	   * @return
-	   */
-	  public static double nCr(int n, int r) {
-		  double num = 1;
-		  for(int i=n; i>(n-r); i--) {
-			  num*=i;
-		  }
-		  double den = 1;
-		  for(int i=r; i>1; i--) {
-			  den*=i;
-		  }
-		  return num/(double)den;		  
-	  }
-	  
-	  public static void main(String[] args) {
-		  System.out.println(getProbability(100,2));
-	  }
-	
+	}
+
 }
