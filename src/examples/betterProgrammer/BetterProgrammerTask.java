e903a09ff05ef0375ad940153ab340cc11f0bf37
sb <2sbsbsb@gmail.com>
2015-04-21 12:57:35 -0700

    BetterProgrammerTask traverseTreeInDepth

--- src/examples/betterProgrammer/BetterProgrammerTask.java
+++ src/examples/betterProgrammer/BetterProgrammerTask.java
@@ -4,30 +4,29 @@ import java.util.*;
 
 public class BetterProgrammerTask {
 
-	public static void sortIgnoringSpaces(String[] a) {
+	// Please do not change this interface
+    public static interface Node {
+        int getValue();
+        List<Node> getChildren();
+    }
+
+
+    public static List<Node> traverseTreeInDepth(Node root) {
         /*
           Please implement this method to
-          sort a given array of Strngs in alphabetical order
-          ignoring spaces (' ' symbols) within the strings.
+          traverse the tree in depth and return a list of all passed nodes.
+
+          The method shall work optimally with large trees.
          */
-
-		Arrays.sort(a, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.replaceAll(" ", "").compareTo(o2.replaceAll(" ", ""));
-			}
-		});
-    }
+    	List<Node> results = new ArrayList<Node>();
+    	if(root!=null) {
+    		results.add(root);
+    		List<Node> childrens = root.getChildren();
+    		for(Node children : childrens) {
+    			results.addAll(traverseTreeInDepth(children));
+    		}
+    	}    	
+    	return results;
+    } 
 	  
-	  
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String[] values = new String[] {"b", " a", "ab", "a c", "z ", "b"};
-		System.out.println(Arrays.toString(values));
-		sortIgnoringSpaces(values);
-		System.out.println(Arrays.toString(values));
-	}
-
 }
