c26532b42ffe7dc553f32bd45a037e0b3ca119f2
sb <2sbsbsb@gmail.com>
2015-04-21 13:09:41 -0700

    BetterProgrammerTask getProbability

--- src/examples/betterProgrammer/BetterProgrammerTask.java
+++ src/examples/betterProgrammer/BetterProgrammerTask.java
@@ -4,29 +4,42 @@ import java.util.*;
 
 public class BetterProgrammerTask {
 
-	// Please do not change this interface
-    public static interface Node {
-        int getValue();
-        List<Node> getChildren();
+	public static double getProbability(int Y, int X) {
+        /*
+          If you roll Y standard six-sided dice, what’s the probability that you get at least X 4s?
+          To calculate that you should divide the number of comibnations with X or more 4s
+          by the total number of possible combinations.
+         */		   	
+	  double sum = 0;
+	  for(int i=X;i<=Y;i++) {
+		 double value = nCr(Y,i) * Math.pow((1/6d),i) * Math.pow(5/6d, Y-i);
+		sum+= (value);
+	  }
+	  return sum;		   
     }
 
+	
 
-    public static List<Node> traverseTreeInDepth(Node root) {
-        /*
-          Please implement this method to
-          traverse the tree in depth and return a list of all passed nodes.
-
-          The method shall work optimally with large trees.
-         */
-    	List<Node> results = new ArrayList<Node>();
-    	if(root!=null) {
-    		results.add(root);
-    		List<Node> childrens = root.getChildren();
-    		for(Node children : childrens) {
-    			results.addAll(traverseTreeInDepth(children));
-    		}
-    	}    	
-    	return results;
-    } 
+	  /**
+	   * @param n
+	   * @param r
+	   * @return
+	   */
+	  public static double nCr(int n, int r) {
+		  double num = 1;
+		  for(int i=n; i>(n-r); i--) {
+			  num*=i;
+		  }
+		  double den = 1;
+		  for(int i=r; i>1; i--) {
+			  den*=i;
+		  }
+		  return num/(double)den;		  
+	  }
+	  
+	  public static void main(String[] args) {
+		  System.out.println(getProbability(100,20));
+	  }
+	
 	  
 }
