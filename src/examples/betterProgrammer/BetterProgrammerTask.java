9ff68d9520a16fc7532c15be4a72a1f7243ebb75
sb <2sbsbsb@gmail.com>
2015-04-21 09:28:47 -0700

    BetterProgrammerTask getPerfectNumbers

--- src/examples/betterProgrammer/BetterProgrammerTask.java
+++ src/examples/betterProgrammer/BetterProgrammerTask.java
@@ -1,30 +1,46 @@
 package examples.betterProgrammer;
 
+import java.util.*;
+
 public class BetterProgrammerTask {
 
-	public static int getSumOfNumbers(String s) {
+	public static List<Integer> getPerfectNumbers(int from, int to) {
 		/*
-		 * Please implement this method to return the sum of all integers found
-		 * in the parameter String. You can assume that integers are separated
-		 * from other parts with one or more spaces (' ' symbol). For example,
-		 * s="12 some text 3  7", result: 22 (12+3+7=22)
+		 * Please implement this method to return a list of all perect numbers
+		 * in the given range inclusively. A perfect number is defined as a
+		 * positive integer which is the sum of its positive divisors not
+		 * including the number itself. For example: 6 is a perfect number
+		 * because 6 = 1 + 2 + 3 (1, 2, 3 are divisors of 6) 28 is also a
+		 * perfect number: 28 = 1 + 2 + 4 + 7 + 14
 		 */
-		String[] values = s.split(" ");
-		int sum = 0;
-		for(String value : values) {
-			int val = 0;
-			try {
-				val = Integer.parseInt(value);
-			} catch(Exception ex) {
-				
+		List<Integer> results = new ArrayList<Integer>();
+		for (int i = from; i <= to; i++) {
+			if (perfectNumber(i)) {
+				results.add(i);
 			}
-			sum+=val;
 		}
-		return sum;
+		return results;
+	}
+	
+	/**
+	 * @param i
+	 * @return
+	 */
+	private static boolean perfectNumber(int num) {
+		int sum=0;
+		for(int i=1;i<num;i++) {
+			if(num%i==0) {
+				sum+=i;
+			}
+		}
+		return sum==num;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(getSumOfNumbers("12 some text 3  7"));
+		List<Integer> results = getPerfectNumbers(1, 50);
+		for(Integer result : results) {
+			System.out.println(result);
+		}
 	}
 
 }
