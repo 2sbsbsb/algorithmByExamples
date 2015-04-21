a0a58a8083e83462c7cafaf23814210d0aa973bb
sb <2sbsbsb@gmail.com>
2015-04-21 12:52:47 -0700

    BetterProgrammerTask - Sort ignoring spaces

--- src/examples/betterProgrammer/BetterProgrammerTask.java
+++ src/examples/betterProgrammer/BetterProgrammerTask.java
@@ -4,37 +4,30 @@ import java.util.*;
 
 public class BetterProgrammerTask {
 
-	  /**
-	   * @param a
-	   * @return
-	   */
-	  public static int[] retainPositiveNumbers(int[] a) {
-	        /*
-	          Please implement this method to
-	          return a new array with only positive numbers from the given array.
-	          The elements in the resulting array shall be sorted in the ascending order.
-	         */
-		  List<Integer> results = new ArrayList<Integer>();
-		  for(int aa : a) {
-			  if(aa>0) {
-				  results.add(aa);
-			  }
-		  }
-		  Collections.sort(results);
-		  int[] values = new int[results.size()];
-		  for(int i=0;i<results.size();i++) {
-			  values[i] = results.get(i);
-		  }
-		  return values;
-	   }
-	  
+	public static void sortIgnoringSpaces(String[] a) {
+        /*
+          Please implement this method to
+          sort a given array of Strngs in alphabetical order
+          ignoring spaces (' ' symbols) within the strings.
+         */
+
+		Arrays.sort(a, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.replaceAll(" ", "").compareTo(o2.replaceAll(" ", ""));
+			}
+		});
+    }
 	  
 	  
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(retainPositiveNumbers(new int[]{1,2,3,5,6,-1,-10,-2, 2})));
+		String[] values = new String[] {"b", " a", "ab", "a c", "z ", "b"};
+		System.out.println(Arrays.toString(values));
+		sortIgnoringSpaces(values);
+		System.out.println(Arrays.toString(values));
 	}
 
 }
